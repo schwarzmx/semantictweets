489a54f28a51a8137deb002be20183f7919a1912
Fernando Hernandez <fher69@gmail.com>
2012-03-08 22:27:10 +0100

    Modified LSI semantic representation. Added KMeans algorithm.

---
+++ semantictweets/kmeans.py
@@ -0,0 +1,150 @@
+# -*- coding: utf-8 -*-
+
+from scipy import dot, mat, linalg, array, random, add
+from math import *
+
+class KMeans:
+    """ This class represents the K-Means clustering algorithm.
+    It takes the semantic space from the LSI model and computes
+    the clustering algorithm.
+    """
+
+    dimensions = -1
+    documents = []
+
+    def __init__(self, sem_space):
+
+        rows, cols = sem_space.shape
+        self.dimensions = rows
+
+        self.documents = [Document(sem_space[:, c], c) for c in xrange(0,cols)]
+
+
+    def cluster(self, k=3, max_iter=10):
+        """ Perform K-Means algorithm.
+        K is the number of clusters. It is done for a max of
+        max_iter iterations.
+        """
+
+        # create a set of k random clusters as seeds
+        old_clusters = [None] * k # just a placeholder
+        clusters = self.random_clusters(k)
+
+        iter = 0
+        while (iter < max_iter) and not (old_clusters == clusters):
+            print "iteration %d..." % iter
+            # assign new clusters to old clusters
+            for i in xrange(0, k):
+                old_clusters[i] = clusters[i]
+                clusters[i].documents = []
+
+            # for each document
+            for document in self.documents:
+
+                # determine the cluster with the highest similarity
+                similarities = [cosine_similarity(document, cluster) for cluster in old_clusters]
+                max_index = array(similarities).argmax()
+
+                print "max_index: %d " % max_index
+
+                # assign document to that cluster
+                clusters[max_index].add(document)
+
+            # update cluster means
+            for cluster in clusters:
+                cluster.update_centroid()
+            
+            iter += 1
+
+
+        return clusters
+
+    def random_clusters(self, k):
+        clusters = []
+
+        for i in range(0,k):
+            clusters.append(Cluster(i,self.dimensions))
+
+        return clusters
+
+class Cluster:
+    """ This class represents a cluster.
+    Each cluster contains an N dimensional vector, where N is the
+    number of documents.
+    """
+
+    # to keep a tag for each cluster
+    index = -1
+
+    # a list of documents attached to this cluster
+    documents = []
+
+    # the mean of the cluster
+    centroid = None
+
+    def __init__(self, tag, dimensions):
+        self.index = tag
+        self.centroid = array(random.standard_normal(dimensions))
+
+        print "new centroid: " + str(self.centroid)
+
+    def add(self, document):
+        self.documents.append(document)
+
+    def update_centroid(self):
+        """ Update the mean of the current cluster """
+
+        total_docs = len(self.documents)
+
+        if total_docs == 0:
+            return
+
+        new_centroid = self.centroid.copy()
+        for document in self.documents:
+            new_centroid = new_centroid + document.vector
+
+        self.centroid = new_centroid / float(total_docs)
+        print self.centroid
+
+    def __eq__(self, other):
+        """ Override rich comparison method.
+        It compares the indices of the documents in the self.documents list.
+        Returns True if the indices are the same in both documents lists, 
+        False otherwise.
+        """
+        if not self or not other: #either one of them is null
+            return False
+
+        if len(self.documents) != len(other.documents):
+            return False
+
+        for i in xrange(0, self.documents):
+            print "this: %d, other: %d" % (self.documents[i].index, other.documents[i].index)
+            if self.documents[i].index != other.documents[i].index:
+                return False
+
+        return True
+
+
+class Document:
+
+    # the internal model of the document
+    vector = None 
+    # for labeling the vectors
+    index = -1  
+
+    def __init__(self, vector, index):
+        self.vector = vector.copy().T
+        self.index = index
+
+def cosine_similarity(document, cluster):
+    """ Helper function for calculating cosine similarities between 
+    a document vector and a cluster centroid.
+    """
+    num = dot(document.vector, cluster.centroid)
+    den = linalg.norm(document.vector) * linalg.norm(cluster.centroid)
+
+    return num / den
+
+
+        
--- semantictweets/lsi.py
+++ semantictweets/lsi.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+
 from scipy import dot, mat, linalg, array
 from math import *
 from pprint import pprint
@@ -12,9 +13,14 @@ class LSI:
     """
 
     vspace = None
+
+    # results from the rank-reduced SVD
     sigma_k = None
-    U_k = None
-    VT_k = None
+    T_k = None
+    D_k = None
+
+    # the semantic space is the representation we are going to use for
+    # k-means, which is sigma_k * D_k
     sem_space = None
 
     def __init__(self, vspace):
@@ -51,47 +57,65 @@ class LSI:
         """ Computes the rank reduced SVD.
         This method computes the SVD of a truncated space for the K highest
         singular values of sigma.
-        First, the SVD for the current vector space is computed, then
-        the top k values of sigma are selected, then the semantic
-        space is computed from the reduced sigma.
+        We transpose the Vector Space obtained in the previous steps to obtain
+        the usual form for LSI. Then the SVD is computed over the space
+        to obtain the Terms, Singular and Document components (matrices).
+        Then those are truncated to obtain the reduced space.
+        An additional step is to compute the semantic space for the documents,
+        which is multiplying Sigma_k * D_k, which will be used by K-Means to
+        cluster the documents by similarity of terms.
         """
 
-        rows, cols = self.vspace.shape
+        rows, cols = self.vspace.T.shape
 
         if k <= rows: # vspace must be truncatable, i.e. k > num_docs
 
             # compute the regular SVD
-            U, s, VT = linalg.svd(self.vspace) 
+            # use the transpose to get the form of X as it is usual
+            # with document vectors and term rows
+            U, s, VT = linalg.svd(self.vspace.T) 
 
-            print "sigmas: " + " length : %d" % len(s)
-            print str(s)
-            # ...conveniently scipy already sorts the singular values
+            
+            # one approach is to truncate the SVD and then multiplying
+            # truncating has the advantage of being computationally simpler
             # we just slice sigma
-            self.sigma_k = s[0:k].copy() # numpy slices are by reference
-            self.U_k = U[0:k].copy()
-            self.VT_k = VT[0:k].copy()
+            self.sigma_k = s[0:k].copy() # in numpy slices are by reference
+            self.T_k = U[:,0:k].copy() # eliminate last columns
+            self.D_k = VT[0:k].copy() # eliminate last rows
 
-            print str(self.sigma_k)
+#            print "Sigmas: " 
+#            print str(s)
+#            print str(self.sigma_k)
+#
+#            print "Terms: "
+#            print prettify(U)
+#            print prettify(self.T_k)
+#
+#            print "Documents: "
+#            print prettify(VT)
+#            print prettify(self.D_k)
 
-            print "Us: "
-            print prettify(U)
-            print prettify(self.U_k)
-            print "VTs: "
-            print prettify(VT)
-            print prettify(self.VT_k)
+            S = linalg.diagsvd(self.sigma_k, k, k)
+            reduced_space = dot(dot(self.T_k, S), self.D_k)
 
-#            # truncate sigma by making zero the lower values
+            self.sem_space = dot(S, self.D_k)
+
+#            # the other approach is to zero out the lowest values of sigma
+#            # conveniently scipy already sorts the singular values
+#            # zeroing out the lowest values of sigma produces
 #            for i in xrange(rows - k, rows):
 #                s[i] = 0
 #
-#            # compute truncated matrix to obtain the semantic space
-#            sem_space = dot(dot(U, linalg.diagsvd(s, len(self.vspace), len(VT))), VT)
-#
-#            self.vspace = sem_space
+#            # compute reduced matrix to obtain the rank-reduced vector space
+#            S = linalg.diagsvd(s, len(self.vspace), len(VT))
+#            reduced_space = dot(dot(U, S), VT)
+#            self.sem_space = dot(S, VT)
+
+            self.vspace = reduced_space
             
 
         else:
-            raise Exception('K must be smaller than the number of documents!')
+            raise Exception('K must be smaller than the number of terms!')
 
 
 
